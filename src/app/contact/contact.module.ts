2fe2adb292276f000fa8318390cf7f1bdbe9fde6
johnj974 <jjdug@hotmail.com>
2022-08-05 12:19:05 +0100

    sharedModule+contactBannerCreated

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactLayoutComponent } from './contact-layout/contact-layout.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ContactLayoutComponent
-  ],
-  imports: [
-    CommonModule,
-    ContactRoutingModule
-  ]
+  declarations: [ContactLayoutComponent],
+  imports: [CommonModule, ContactRoutingModule, SharedModule],
 })
-export class ContactModule { }
+export class ContactModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactBannerComponent } from './contact-banner/contact-banner.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactBannerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ContactBannerComponent
+  ]
+})
+export class SharedModule { }
