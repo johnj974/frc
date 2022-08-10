5c068ebd054bc4b390f8a3a7406e9e9cc9d994fe
johnj974 <jjdug@hotmail.com>
2022-08-10 12:35:15 +0100

    errorValidation+messagesAddedToContactForm

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClassesService } from 'src/app/services/classes.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,19 +10,25 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ContactFormComponent implements OnInit {
   //.
   contactForm: FormGroup;
+  availableClasses = [];
 
   onSubmit() {
     console.log(this.contactForm);
   }
 
-  constructor() {}
+  constructor(private classeService: ClassesService) {}
 
   ngOnInit(): void {
+    this.availableClasses = this.classeService.classesAvailableArray;
+
     this.contactForm = new FormGroup({
-      contactName: new FormControl(null),
-      email: new FormControl(null),
-      topic: new FormControl(null),
-      message: new FormControl(null),
+      contactName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      topic: new FormControl(null, Validators.required),
+      message: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(100),
+      ]),
     });
   }
 }
