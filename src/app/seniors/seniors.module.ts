67ff8bf01bf85f628be8da6b4c71933e768e8c08
johnj974 <jjdug@hotmail.com>
2022-08-16 12:53:16 +0100

    topBannerComponentCreated/StartSeniorPage

--- src/app/seniors/seniors.module.ts
+++ src/app/seniors/seniors.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SeniorsRoutingModule } from './seniors-routing.module';
 import { SeniorsLayoutComponent } from './seniors-layout/seniors-layout.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    SeniorsLayoutComponent
-  ],
-  imports: [
-    CommonModule,
-    SeniorsRoutingModule
-  ]
+  declarations: [SeniorsLayoutComponent],
+  imports: [CommonModule, SeniorsRoutingModule, SharedModule],
 })
-export class SeniorsModule { }
+export class SeniorsModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactBannerComponent } from './contact-banner/contact-banner.component';
-
-
+import { TopBannerComponent } from './top-banner/top-banner.component';
 
 @NgModule({
-  declarations: [
-    ContactBannerComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    ContactBannerComponent
-  ]
+  declarations: [ContactBannerComponent, TopBannerComponent],
+  imports: [CommonModule],
+  exports: [ContactBannerComponent, TopBannerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
