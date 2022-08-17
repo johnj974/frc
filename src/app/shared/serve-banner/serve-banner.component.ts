44caf423ffa39429377a3e7f2a804116b849c32f
johnj974 <jjdug@hotmail.com>
2022-08-17 12:24:15 +0100

    serveComponantCreated+addedToSeniorLayout

---
+++ src/app/shared/serve-banner/serve-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-serve-banner',
+  templateUrl: './serve-banner.component.html',
+  styleUrls: ['./serve-banner.component.scss']
+})
+export class ServeBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ContactBannerComponent } from './contact-banner/contact-banner.component';
 import { TopBannerComponent } from './top-banner/top-banner.component';
 import { InfoBannerComponent } from './info-banner/info-banner.component';
+import { ServeBannerComponent } from './serve-banner/serve-banner.component';
 
 @NgModule({
   declarations: [
     ContactBannerComponent,
     TopBannerComponent,
     InfoBannerComponent,
+    ServeBannerComponent,
   ],
   imports: [CommonModule],
-  exports: [ContactBannerComponent, TopBannerComponent, InfoBannerComponent],
+  exports: [
+    ContactBannerComponent,
+    TopBannerComponent,
+    InfoBannerComponent,
+    ServeBannerComponent,
+  ],
 })
 export class SharedModule {}
