23f9490c83b3dec2b2ea1cc6db4d1a7f8e7c23e2
johnj974 <jjdug@hotmail.com>
2022-08-17 09:44:55 +0100

    infoBannerCreated+addedToSeniorComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactBannerComponent } from './contact-banner/contact-banner.component';
 import { TopBannerComponent } from './top-banner/top-banner.component';
+import { InfoBannerComponent } from './info-banner/info-banner.component';
 
 @NgModule({
-  declarations: [ContactBannerComponent, TopBannerComponent],
+  declarations: [
+    ContactBannerComponent,
+    TopBannerComponent,
+    InfoBannerComponent,
+  ],
   imports: [CommonModule],
-  exports: [ContactBannerComponent, TopBannerComponent],
+  exports: [ContactBannerComponent, TopBannerComponent, InfoBannerComponent],
 })
 export class SharedModule {}
