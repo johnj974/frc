fb1c5ba404b58d4bc1498d63be07cae49676c89c
johnj974 <jjdug@hotmail.com>
2022-08-18 14:11:38 +0100

    MainContentForServicesLandingPageAdded

--- src/app/resources/resources-display/resources-display.component.ts
+++ src/app/resources/resources-display/resources-display.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OffersModel } from 'src/app/models/offers.model';
+import { OffersService } from 'src/app/services/offers.service';
 
 @Component({
   selector: 'app-resources-display',
   templateUrl: './resources-display.component.html',
-  styleUrls: ['./resources-display.component.scss']
+  styleUrls: ['./resources-display.component.scss'],
 })
 export class ResourcesDisplayComponent implements OnInit {
+  //.
+  servicesProvidedArray: OffersModel[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toPage(link: string) {
+    console.log(link);
   }
 
+  constructor(private offersService: OffersService) {}
+
+  ngOnInit(): void {
+    this.servicesProvidedArray = this.offersService.retrieveOffersArray();
+  }
 }
