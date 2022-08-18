91176f8765dab16732bd098cd82d9d359fa0376e
johnj974 <jjdug@hotmail.com>
2022-08-18 09:40:22 +0100

    offersModel+ServiceCreated

---
+++ src/app/models/offers.model.ts
@@ -0,0 +1,8 @@
+export class OffersModel {
+  constructor(
+    public name: string,
+    public description: string,
+    public link: string,
+    public image: string
+  ) {}
+}
---
+++ src/app/services/offers.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { OffersModel } from '../models/offers.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OffersService {
+  //.
+
+  offersArray: OffersModel[] = [
+    new OffersModel(
+      'DIY',
+      'We aim to make home repairs affordable, allowing vulnerable members of our community to live independently.',
+      'diy',
+      './assets/images/lumber.jpg'
+    ),
+    new OffersModel(
+      'Laundry',
+      'We offer a range of services including washing, drying, ironing and dry cleaning service is accommodated. ',
+      'laundry',
+      './assets/images/washing-machine.jpg'
+    ),
+    new OffersModel(
+      'Creche',
+      'Little Voyagers Childcare comprises a crèche, pre-school & after school club!',
+      'creche',
+      './assets/images/paint-brushes.jpg'
+    ),
+  ];
+
+  retrieveOffersArray() {
+    return this.offersArray.slice();
+  }
+
+  constructor() {}
+}
+
+// Photo washing machines by Ekaterina Belinskaya @ pexels
+// Photo paint-brushes by Anna Kolosyuk @ unsplash
+// Photo lumber by Marissa Daeger @ unsplash
