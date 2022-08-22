481c31cbdec7c94e92fbe02bdc195f80d3c95951
johnj974 <jjdug@hotmail.com>
2022-08-22 15:09:48 +0100

    basicNext+PrevBtnsAddedToSingleClassDisplayPage

--- src/app/classes/classes-single/classes-single.component.ts
+++ src/app/classes/classes-single/classes-single.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ClassModel } from 'src/app/models/class.model';
 import { ClassesService } from 'src/app/services/classes.service';
@@ -15,11 +15,37 @@ export class ClassesSingleComponent implements OnInit, OnDestroy {
   chosenClass: ClassModel;
   classId: number;
   classSubscription: Subscription;
-  classGoals:string[];
+  classGoals: string[];
+  classesArray: ClassModel[];
+  disableNextButton = false;
+  disablePrevButton = false;
+
+  toNextClass() {
+    console.log(this.classId++);
+    let nextClass = this.classId++;
+    let newClass = this.classesService.retrieveSingleClass(nextClass);
+    let className = newClass.name.replace(/ /g, '');
+    this.router.navigate(['/classes', nextClass, className]);
+    if (nextClass >= this.classesArray.length - 1) {
+      this.disableNextButton = true;
+    }
+  }
+
+  toPreviousClass() {
+    console.log(this.classId--);
+    let prevClass = this.classId--;
+    let newClass = this.classesService.retrieveSingleClass(prevClass);
+    let className = newClass.name.replace(/ /g, '');
+    this.router.navigate(['/classes', prevClass, className]);
+    if (prevClass >= this.classesArray.length - 1) {
+      this.disablePrevButton = true;
+    }
+  }
 
   constructor(
     private route: ActivatedRoute,
-    private classesService: ClassesService
+    private classesService: ClassesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -27,6 +53,7 @@ export class ClassesSingleComponent implements OnInit, OnDestroy {
       this.classId = params.id;
       this.chosenClass = this.classesService.retrieveSingleClass(this.classId);
       this.classGoals = this.chosenClass.goal;
+      this.classesArray = this.classesService.retrieveClassArray();
     });
   }
 
