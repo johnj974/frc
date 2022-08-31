3669b53d481d7c397dc8cde9b26c667a324159d0
johnj974 <jjdug@hotmail.com>
2022-08-31 14:41:27 +0100

    contentAddedToTwoServicesForSingleServiceDisplay

--- src/app/services/offers.service.ts
+++ src/app/services/offers.service.ts
@@ -24,48 +24,134 @@ export class OffersService {
       'We aim to make home repairs affordable, allowing vulnerable members of our community to live independently.',
       'diy',
       './assets/images/lumber.jpg',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      '',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      ''
+      'D.I.Y',
+      'We aim to make home repairs affordable, allowing vulnerable members of our community to live independently in their own homes in a safe and secure environment.',
+      'This is one of our more used services as we provide the labour for a lot of small jobs. Painting, decorating, gardening, power-hosing jobs are within our range. Our clients are comfortable with our staff and are reassured that the job will be done economically and to a high standard.',
+      [
+        {
+          title: 'Wash & Dry per 10kg load',
+          price: '15',
+        },
+        {
+          title: 'Dry only per 10kg load',
+          price: '8',
+        },
+        {
+          title: 'Wash only per 10kg load',
+          price: '8',
+        },
+      ],
+      './assets/images/tools.jpg',
+      'GRASS CUTTING',
+      'We offer a lawn cutting service to vulnerable members of our community. Let us help you keep your garden trim and in order over the summer months.',
+      'Let us help you. We offer very good rates.',
+      [
+        {
+          title: 'Small Lawn',
+          price: '20',
+        },
+        {
+          title: 'Medium Lawn',
+          price: '35',
+        },
+        {
+          title: 'Large Lawn',
+          price: '50',
+        },
+      ],
+      './assets/images/lawn-mower.jpg'
     ),
     new OffersModel(
       'Laundry',
       'We offer a range of services including washing, drying, ironing and dry cleaning service is accommodated. ',
       'laundry',
       './assets/images/washing-machine.jpg',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      '',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      ''
+      'Wash and dry',
+      'We will wash, dry and fold your clothes, towels, bed linen etc.',
+      'All your items are ready to be put straight into the hot-press or chest of drawers. We also offer a wash only or dry only service',
+      [
+        {
+          title: 'Wash & Dry per 10kg load',
+          price: '15',
+        },
+        {
+          title: 'Dry only per 10kg load',
+          price: '8',
+        },
+        {
+          title: 'Wash only per 10kg load',
+          price: '8',
+        },
+      ],
+      './assets/images/washing-machines.jpg',
+      'Ironing',
+      'Tired of ironing?',
+      'Let us help you. We offer very good rates.',
+      [
+        {
+          title: 'Shirts',
+          price: '1',
+        },
+        {
+          title: 'Pantss',
+          price: '2',
+        },
+        {
+          title: 'Sheets',
+          price: '2',
+        },
+        {
+          title: 'Duvet covers',
+          price: '2',
+        },
+      ],
+      './assets/images/iron.jpg'
     ),
     new OffersModel(
       'Creche',
       'Little Voyagers Childcare comprises a crèche, pre-school & after school club!',
       'creche',
       './assets/images/paint-brushes.jpg',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      '',
-      '',
-      '',
-      '',
-      [{}, {}, {}, {}],
-      ''
+      'Wash and dry',
+      'We will wash, dry and fold your clothes, towels, bed linen etc.',
+      'All your items are ready to be put straight into the hot-press or chest of drawers. We also offer a wash only or dry only service',
+      [
+        {
+          title: 'Wash & Dry per 10kg load',
+          price: '15',
+        },
+        {
+          title: 'Dry only per 10kg load',
+          price: '8',
+        },
+        {
+          title: 'Wash only per 10kg load',
+          price: '8',
+        },
+      ],
+      './assets/images/lumber.jpg',
+      'Ironing',
+      'Tired of ironing?',
+      'Let us help you. We offer very good rates.',
+      [
+        {
+          title: 'Shirts',
+          price: '1',
+        },
+        {
+          title: 'Pantss',
+          price: '2',
+        },
+        {
+          title: 'Sheets',
+          price: '2',
+        },
+        {
+          title: 'Duvet covers',
+          price: '2',
+        },
+      ],
+      './assets/images/lumber.jpg'
     ),
   ];
 
@@ -83,3 +169,7 @@ export class OffersService {
 // Photo washing machines by Ekaterina Belinskaya @ pexels
 // Photo paint-brushes by Anna Kolosyuk @ unsplash
 // Photo lumber by Marissa Daeger @ unsplash
+// photo washing-machines by Anca Gabriela Zosin @ unsplash
+// photo iron by Filip Mroz
+// photo lawn-mower by Daniel Watson
+// Photo tools by Barn Images
